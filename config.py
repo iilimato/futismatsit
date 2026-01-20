d0df9355d404eac9a192b133032cd34555647462
Eelis Aarni Kaleva Lehtinen <56164889+iilimato@users.noreply.github.com>
2026-01-20 20:54:48 +0200

    Add secret key to config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-import secrets
+# import secrets
+secret_key = "18fd24bf6a2ad4dac04a33963db1c42f"
 
-secret_key = secrets.token_hex(16)
+
+# In production we could use this:
+# secret_key = secrets.token_hex(16)
