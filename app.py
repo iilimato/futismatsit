18151f973d89c670ca5f9e51ad6f05d3bb82cbc2
Eelis Aarni Kaleva Lehtinen <56164889+iilimato@users.noreply.github.com>
2026-01-20 20:31:51 +0200

    Add register and user creation routes to app.py

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import db
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
