d962006e43e461dcdb21b45267bfa924eca067fd
Eelis Aarni Kaleva Lehtinen <56164889+iilimato@users.noreply.github.com>
2026-01-23 13:39:50 +0200

    Edit app.py so that it saves also user_id

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/new_game")
+def new_game():
+    return render_template("new_game.html")
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -46,10 +51,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
