2576f25bcdbc7da5e00415aaf1b830128be6a383
Eelis Aarni Kaleva Lehtinen <56164889+iilimato@users.noreply.github.com>
2026-01-23 13:32:23 +0200

    Add games table to the database schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,14 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    location TEXT,
+    game_time TEXT,
+    players_needed INTEGER,
+    user_id INTEGER REFERENCES users,
+    created_at TEXT
+);
