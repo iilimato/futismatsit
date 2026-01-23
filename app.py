f187f9613a7441a3cc5468ce7d4f39a3a87aaf8f
Eelis Aarni Kaleva Lehtinen <56164889+iilimato@users.noreply.github.com>
2026-01-23 13:22:05 +0200

    Add route for index.html

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 app.secret_key = config.secret_key
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
