fecefdd05a8bcf678d0a66cb63f29a7963a10025
Eelis Aarni Kaleva Lehtinen <56164889+iilimato@users.noreply.github.com>
2026-03-01 10:37:08 +0200

    Fix some pylint errors.

--- seed.py
+++ seed.py
@@ -9,20 +9,22 @@ db.execute("DELETE FROM game_levels")
 db.execute("DELETE FROM games")
 db.execute("DELETE FROM users")
 
-user_count = 1000
-game_count = 10**5
-comment_count = 10**6
+USER_COUNT = 1000
+GAME_COUNT = 10**5
+COMMENT_COUNT = 10**6
 
-locations = ["Lahden kisapuisto", "Töölön Pallokenttä 3", "Väinämöisen kenttä", "Tammelan stadion", "Kupittaan alakenttä",
-             "Arabianrannan kenttä", "Laaksolahden jalkapallohalli", "Kuopion keskuskenttä", "Tikkurilan urheilupuisto", "Tapiolan urheilupuisto"]
+locations = ["Lahden kisapuisto", "Töölön Pallokenttä 3", "Väinämöisen kenttä",
+             "Tammelan stadion", "Kupittaan alakenttä",
+             "Arabianrannan kenttä", "Laaksolahden jalkapallohalli", "Kuopion keskuskenttä",
+             "Tikkurilan urheilupuisto", "Tapiolan urheilupuisto"]
 levels = ["Aloittelijat", "Keskitaso", "Edistyneet"]
 
-for i in range(1, user_count + 1):
+for i in range(1, USER_COUNT + 1):
     db.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)",
                ["user" + str(i), "hash" + str(i)])
 
-for i in range(1, game_count + 1):
-    user_id = random.randint(1, user_count)
+for i in range(1, GAME_COUNT + 1):
+    user_id = random.randint(1, USER_COUNT)
     location = random.choice(locations)
     player_count = random.choice([6, 8, 10, 12, 14, 22])
     db.execute("""INSERT INTO games (title, description, location, date, time,
@@ -36,9 +38,9 @@ for i in range(1, game_count + 1):
     db.execute("""INSERT INTO game_levels (level, game_id)
                   VALUES (?, ?)""", [level, i])
 
-for i in range(1, comment_count + 1):
-    user_id = random.randint(1, user_count)
-    game_id = random.randint(1, game_count)
+for i in range(1, COMMENT_COUNT + 1):
+    user_id = random.randint(1, USER_COUNT)
+    game_id = random.randint(1, GAME_COUNT)
     db.execute("""INSERT INTO comments (content, sent_at, user_id, game_id)
                   VALUES (?, datetime('now'), ?, ?)""",
                ["kommentti" + str(i), user_id, game_id])
